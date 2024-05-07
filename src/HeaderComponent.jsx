a37381d3ef33c2b0cf48d4f5cf81169180a1314f
leoocaba <leoo.caballero@gmail.com>
2024-05-07 19:15:13 -0300

    header and footer

---
+++ src/HeaderComponent.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const HeaderComponent = () => {
+  return (
+    <header className="container">
+      <div className="row m-0 p-0">
+        <div className="d-flex align-content-center p-0 m-0">
+          <div className="py-2">
+            <img className="" src="src\assets\icon.png" alt="Nuestro clima" />
+          </div>
+          <div className="my-auto py-2 ">
+            <h1 className="m-0 p-0">Nuestro Clima</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import WeatherApp from "./WeatherApp";
+import { HeaderComponent } from "./HeaderComponent";
+import { FooterComponent } from "./FooterComponent";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {" "}
-    <WeatherApp />{" "}
+    <>
+      <HeaderComponent />
+      <WeatherApp />
+      <FooterComponent />
+    </>
   </React.StrictMode>
 );
