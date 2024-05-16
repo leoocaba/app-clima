ef87143f3df4194c25af0d87796cfbb3430fb67a
leoocaba <leoo.caballero@gmail.com>
2024-05-15 23:00:39 -0300

    Corrección de menejo de errores en la búsqueda

--- src/FooterComponent.jsx
+++ src/FooterComponent.jsx
@@ -3,8 +3,10 @@ import React from "react";
 export const FooterComponent = () => {
   return (
     <footer className="bg-dark text-white py-4">
-      <div className="container text-center">
-        <p className="mb-0">Hecho con ❤️ por ti, para ti</p>
+      <div className="container text-center align-items-center">
+        <p className="d-flex justify-content-center m-0 p-0">
+          Hecho con ❤️ por <span className="mx-1">@leoocaba</span>
+        </p>
       </div>
     </footer>
   );
--- src/HeaderComponent.jsx
+++ src/HeaderComponent.jsx
@@ -4,20 +4,7 @@ export const HeaderComponent = () => {
   return (
     <header className="bg-dark text-white w-100">
       <div className="container-fluid">
-        <div className="row m-0 p-0">
-          <div className="d-flex align-content-center justify-content p-0 m-0">
-            <div className="col my-auto py-1">
-              <h1 className="m-0 p-1">Nuestro Clima</h1>
-            </div>
-            <div className="col py-1">
-              <img
-                className="p-1"
-                src="src\assets\icon.png"
-                alt="Nuestro clima"
-              />
-            </div>
-          </div>
-        </div>
+        <h1 className="text-uppercase fs-3 m-auto p-3">Nuestro Clima</h1>
       </div>
     </header>
   );
