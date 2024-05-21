a78c197d9ff1f656df6ccd69adc62c40bbc103d4
leoocaba <leoo.caballero@gmail.com>
2024-05-21 00:15:24 -0300

    Corrección de css y footer

--- src/FooterComponent.jsx
+++ src/FooterComponent.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 
 export const FooterComponent = () => {
+  const instagram = "https://instagram.com/leoocaba";
+
   return (
     <footer className="bg-dark text-white py-4">
       <div className="container text-center align-items-center">
         <p className="d-flex justify-content-center m-0 p-0">
-          Hecho con ❤️ por <span className="mx-1">@leoocaba</span>
+          Hecho con ❤️ por{" "}
+          <a
+            className="d-flex d-inline p-0 m-0 mx-1"
+            href={instagram}
+            target="_blank"
+          >
+            @leoocaba
+          </a>
         </p>
       </div>
     </footer>
--- src/HeaderComponent.jsx
+++ src/HeaderComponent.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 export const HeaderComponent = () => {
+  const title = "Nuestro Clima";
+
   return (
     <header className="bg-dark text-white w-100">
       <div className="container-fluid">
-        <h1 className="text-uppercase fs-3 m-auto p-3">Nuestro Clima</h1>
+        <h1 className="text-uppercase fs-3 m-auto p-3"> {title} </h1>
       </div>
     </header>
   );
